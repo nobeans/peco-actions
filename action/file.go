6e6f073e11507ad8ffe86cbdc2ac96e48122f186
Yasuharu Nakano <ynak@jggug.org>
2017-07-14 23:41:29 +0900

    EDITOR's parameter (line number & pattern)

--- action/file.go
+++ action/file.go
@@ -3,9 +3,10 @@ package action
 import (
 	cmn "github.com/nobeans/peco-actions/common"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
+	"regexp"
+	"strconv"
 )
 
 type (
@@ -16,22 +17,23 @@ func (FileActionType) prompt() string {
 	return "file-actions>"
 }
 
-func (FileActionType) menuItems(rawPaths []string) ([]menuItem, error) {
-	quotedPaths := strings.TrimSpace(strings.Join(quoteIfRequired(rawPaths), " "))
+func (FileActionType) menuItems(lines []string) ([]menuItem, error) {
+	paths, lineNumOfFirstFile := linesToPaths(lines)
+
+	quotedPaths := strings.TrimSpace(strings.Join(quoteIfRequired(paths), " "))
 	log.Printf("quotedPaths: %s", quotedPaths)
 
-	expandedQuotedPaths := strings.TrimSpace(strings.Join(quoteIfRequired(expandAllPaths(rawPaths)), " "))
+	expandedQuotedPaths := strings.TrimSpace(strings.Join(quoteIfRequired(expandAllPaths(paths)), " "))
 	log.Printf("expandedQuotedPaths: %s", expandedQuotedPaths)
 
 	items := []menuItem{}
 
 	// Without directories
-	if allFiles(rawPaths) {
-		log.Printf("All file exist and aren't directory: %#v", rawPaths)
-		items = append(items, []menuItem{
-			{Label: "Edit", Action: os.Getenv("EDITOR") + " " + quotedPaths},
-			{Label: "Show text", Action: "cat " + quotedPaths},
-		}...)
+	// TODO only text!!!!
+	if allFiles(paths) {
+		log.Printf("All file exist and aren't directory: %#v", paths)
+		items = append(items, menuItem{Label: "Edit", Action: editorCommandLine(quotedPaths, lineNumOfFirstFile)})
+		items = append(items, menuItem{Label: "Show text", Action: "cat " + quotedPaths})
 	}
 
 	// Only if cwd in git repository and tig exists
@@ -40,12 +42,12 @@ func (FileActionType) menuItems(rawPaths []string) ([]menuItem, error) {
 	}
 
 	// Only for single target
-	if len(rawPaths) == 1 {
-		if cmn.ExistFile(rawPaths[0]) {
-			if cmn.IsDirectory(rawPaths[0]) {
+	if len(paths) == 1 {
+		if cmn.ExistFile(paths[0]) {
+			if cmn.IsDirectory(paths[0]) {
 				items = append(items, menuItem{Label: "Go into", Action: "cd " + quotedPaths})
 			} else {
-				parentDir := filepath.Dir(rawPaths[0])
+				parentDir := filepath.Dir(paths[0])
 				items = append(items, menuItem{Label: "Go to parent", Action: "cd " + parentDir})
 			}
 		}
@@ -60,6 +62,57 @@ func (FileActionType) menuItems(rawPaths []string) ([]menuItem, error) {
 	}...)
 
 	return items, nil
+}
+
+func linesToPaths(lines []string) ([]string, int) {
+	// Support "path:lineNum:lineString" as grep result (lineString is ignored)
+	paths := make([]string, len(lines))
+	lineNumOfFirstFile := -1
+	if len(paths) > 0 && isGrepFormat(lines[0]) {
+		for i, line := range lines {
+			tokens := strings.SplitN(line, ":", 3)
+			paths[i] = tokens[0]
+			if lineNumOfFirstFile < 0 {
+				lineNumOfFirstFile, _ = strconv.Atoi(tokens[1])
+			}
+		}
+	} else {
+		paths = append(paths, lines...)
+	}
+
+	// TODO 重複ファイルパスを除去する
+
+	return paths, lineNumOfFirstFile
+}
+
+func isGrepFormat(line string) bool {
+	return regexp.MustCompile("^[^:]+:[0-9]+:.*$").MatchString(line)
+}
+
+func editorCommandLine(path string, lineNum int) string {
+	cl := make([]string, 0)
+
+	cmd := cmn.Env("EDITOR", "vi")
+	cl = append(cl, cmd)
+
+	// only for vim
+	// If EDITOR is "vi(m)" and line is a grep format, use editor options. Yes, I love vim.
+	if regexp.MustCompile("(vi|vim)$").MatchString(cmd) {
+		// Default line for first file
+		if lineNum > 0 {
+			cl = append(cl, "+" + strconv.Itoa(lineNum))
+		}
+
+		// Highlight in vim
+		pattern := cmn.Env("PECO_ACTIONS_EDITOR_PATTERN", "")
+		if len(pattern) > 0 {
+			cl = append(cl, "+/\"\\c" + pattern + "\"")
+		}
+	}
+
+	cl = append(cl, path)
+
+	return strings.Join(cl, " ")
 }
 
 func expandAllPaths(paths []string) []string {
